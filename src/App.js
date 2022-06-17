7ae70e5b19da73e2493500573aabb8bec5775790
Brian Deitchman <briandeitchman@gmail.com>
2022-06-16 18:26:18 -0700

    Added a display screen and buttons for the calculator

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
+
+
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Picture from './components/Picture';
 import Button from './components/Button';
+import Screen from './components/Screen';
+import { useState } from "react"
 
 
-// next picture
-// delete current pic 
-// create new one
-const nextPicture = async () => {
 
-}
+
 
 
 function App() {
+
+  const [operations, setOperations] = useState([1,'+',1])
+  const addNumber = (e)=>{
+    console.log(e.target.innerText)
+    setOperations( prevOperations =>{
+      return [...prevOperations,e.target.innerText]
+    })
+  }
+  
   return (
-    <body>
+    <div>
       <Header />
-      <div className='pictureHolder'>
-        <Picture className='picture' />
-      </div>
-      <Button color='green' text='Like' />
-      <Button color='red' text='next' />
+      <Screen operations={operations}/>
+      <Button  text='1' onClick={addNumber}/>
+      <Button  text='2' onClick={addNumber}/>
+      <Button  text='3' onClick={addNumber}/>
+      <Button  text='4' onClick={addNumber}/>
+      <Button  text='5' onClick={addNumber}/>
+      <Button  text='6' onClick={addNumber}/>
+      <Button  text='7' onClick={addNumber}/>
+      <Button  text='8' onClick={addNumber}/>
+      <Button  text='9' onClick={addNumber}/>
+      <Button  text='0' onClick={addNumber}/>
+      <Button  text='+' onClick={addNumber} />
+      <Button  text='-' onClick={addNumber} />
+      <Button  text='/' onClick={addNumber} />
+      <Button  text='*' onClick={addNumber} />
+
+      
 
       <Footer />
-    </body>
+    </div>
   );
 }
 
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Button = ({ text, color }) => {
+const Button = ({ text, color, onClick }) => {
+
     return (
 
-        <button style={{ backgroundColor: color }} className='btn'>
+        <button onClick={onClick} style={{ backgroundColor: color }} className='btn'>
             {text}
         </button>
 
